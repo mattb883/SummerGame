d69b1323cde89ee1dd75bb446816f5f81582c5ec
mattb883 <rangerdanger694@gmail.com>
2019-06-28 18:30:17 -0400

    Creation of game screen (unfinished)

--- src/carts/mario/controllers/MarioGame.java
+++ src/carts/mario/controllers/MarioGame.java
@@ -1,4 +1,38 @@
 package carts.mario.controllers;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class MarioGame {
+    private JPanel GameScreen;
+
+    private MarioGame() {
+        //initialization, action events, and static variable declaration here
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("SuperMario");          //create screen
+        frame.setSize(1200, 600);     //set size
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);           //ends program by default
+        frame.getContentPane().add(new MarioGame().GameScreen);        //create panel
+        frame.pack();                                           //physical size or creation of screen stays the same, therefore finalize
+        //frame.setResizable(false);                      //remove ability to resize screen
+        frame.setVisible(true);                             //make screen visible
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
